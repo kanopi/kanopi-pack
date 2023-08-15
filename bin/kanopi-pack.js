32d7cec081b58f9af7ad53555671e936c0781cf4
Ryan Leeson <ryan.leeson@kanopistudios.com>
2023-08-15 16:59:02 -0400

    - Add ability to check the current environment configuration

--- bin/kanopi-pack.js
+++ bin/kanopi-pack.js
@@ -3,25 +3,42 @@
 const chalk = require('chalk');
 const package = require('../index');
 const {
-    commands: { standard: program },
-    configuration: { development, production },
-    runners: { runDevServer, runWebpack }
+  commands: { standard: program },
+  configuration: { development, production },
+  runners: { runDevServer, runWebpack }
 } = package;
 
 program
-    .command('standard')
-    .description('Run Webpack builds, set environment to develoment for HMR')
-    .argument('[environment]', 'Choose production (default) or development')
-    .action((environment) => {
-        const isDevelopment = 'development' === environment;
+  .command('check-configuration')
+  .description('Output the Webpack configuration for the specified environment.')
+  .argument('[environment]', 'Choose production (default) or development')
+  .action((environment) => {
+    const isDevelopment = 'development' === environment;
 
-        console.log(chalk.greenBright('Package:\tKanopi Pack Standard'))
-        console.log(chalk.yellow('Environment:\t' + (isDevelopment ? 'Development' : 'Production')));
-        console.log('');
+    console.log(chalk.greenBright('Package:\tKanopi Pack Standard'))
+    console.log(chalk.yellow('Environment:\t' + (isDevelopment ? 'Development' : 'Production')));
+    console.log(chalk.yellow('Current configuration:'));
+    console.log('');
 
-        isDevelopment 
-            ? runDevServer(development)
-            : runWebpack(production);
-    });
+    isDevelopment
+      ? console.log(development)
+      : console.log(production);
+  });
+
+program
+  .command('standard')
+  .description('Run Webpack builds, set environment to development for HMR')
+  .argument('[environment]', 'Choose production (default) or development')
+  .action((environment) => {
+    const isDevelopment = 'development' === environment;
+
+    console.log(chalk.greenBright('Package:\tKanopi Pack Standard'))
+    console.log(chalk.yellow('Environment:\t' + (isDevelopment ? 'Development' : 'Production')));
+    console.log('');
+
+    isDevelopment
+      ? runDevServer(development)
+      : runWebpack(production);
+  });
 
 program.parse(process.argv);
