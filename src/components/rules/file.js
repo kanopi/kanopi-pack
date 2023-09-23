0abf6ff4d642766680dca442f386f4c94fc3c905
Ryan Leeson <ryan.leeson@kanopistudios.com>
2023-09-22 20:30:50 -0400

    - Include SVG optimizer for inline

--- src/components/rules/file.js
+++ src/components/rules/file.js
@@ -4,6 +4,7 @@
  * @returns {array}
  */
 module.exports = (configuration = {}) => {
+  const svgToMiniDataURI = require('mini-svg-data-uri');
   const {
     assets = {}
   } = configuration;
@@ -13,9 +14,22 @@ module.exports = (configuration = {}) => {
   } = assets;
   const rules = [];
 
-  if (Array.isArray(inline) && 0 < inline.length) {
+  const useSvg = Array.isArray(inline) && -1 < inline.indexOf('svg');
+  const otherInline = Array.isArray(inline) ? inline.filter((item) => 'svg' !== item) : [];
+
+  if (useSvg) {
     rules.push({
-      test: new RegExp('\.(' + inline.join('|') + ')$', 'i'),
+      test: new RegExp('\.svg$', 'i'),
+      type: 'asset/inline',
+      generator: {
+        dataUrl: (content) => svgToMiniDataURI(content.toString())
+      }
+    });
+  }
+
+  if (Array.isArray(otherInline) && 0 < otherInline.length) {
+    rules.push({
+      test: new RegExp('\.(' + otherInline.join('|') + ')$', 'i'),
       type: 'asset/inline'
     });
   }
