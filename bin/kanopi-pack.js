3e83487b2f3486d8b5f1f3e5b0973887d46210e7
Ryan Leeson <ryan.leeson@kanopistudios.com>
2024-03-23 14:32:48 -0400

    - Add Watch to check-configuration

--- bin/kanopi-pack.js
+++ bin/kanopi-pack.js
@@ -20,23 +20,24 @@ program
   .command('check-configuration')
   .description('Output the Webpack configuration for the specified environment.')
   .argument('[depth]', 'Number of levels deep (default 6) to show the configuration')
-  .argument('[environment]', 'Choose production (default) or development')
+  .argument('[environment]', 'Choose production (default), development, or watch')
   .argument('[color]', 'Whether to show the output in color (default false)')
   .action((depth = 6, environment = 'production', color = false) => {
-    const isDevelopment = 'development' === environment;
+    const selectedMode = PACKAGE_MODES[environment] ?? PACKAGE_MODES['production'];
+    const { configuration, name } = selectedMode;
 
     console.log(chalk.greenBright('Package:\tKanopi Pack Standard'))
-    console.log(chalk.yellow('Environment:\t' + (isDevelopment ? 'Development' : 'Production')));
+    console.log(chalk.yellow('Environment:\t' + name));
     console.log('');
     console.log(chalk.yellow('Current configuration:'));
     console.log('');
-    console.log(utility.inspect(isDevelopment ? development : production, { depth: depth, colors: color }));
+    console.log(utility.inspect(configuration, { depth: depth, colors: color }));
   });
 
 program
   .command('standard')
   .description('Run Webpack builds, set environment to development for HMR, or watch for rebuilds without HMR.')
-  .argument('[environment]', 'Choose production (default), watch or development')
+  .argument('[environment]', 'Choose production (default), development, or watch')
   .action((environment = 'production') => {
     const selectedMode = PACKAGE_MODES[environment] ?? PACKAGE_MODES['production'];
     const { configuration, name, runner } = selectedMode;
